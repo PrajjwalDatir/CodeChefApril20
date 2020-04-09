9a4c968cffc5d8e2b0cadf838c2ea53c5871fc1c
Prajjwal Datir <46681482+PrajjwalDatir@users.noreply.github.com>
2020-04-09 23:24:42 +0530

    Update StrangeNumber.py

--- StrangeNumber.py
+++ StrangeNumber.py
@@ -1,66 +1,29 @@
-from sys import stdin
+import sys
+import math
 
 
-def isPrime(n):
-    # Corner cases
-    if n <= 1:
-        return False
-    if n <= 3:
-        return True
-
-    # This is checked so that we can skip
-    # middle five numbers in below loop
-    if n % 2 == 0 or n % 3 == 0:
-        return False
-    i = 5
-    while i * i <= n:
-        if n % i == 0 or n % (i + 2) == 0:
-            return False
-        i = i + 6
-    return True
+def primeFactors(n):
+    count = 0
+    while n % 2 == 0:
+        n = n / 2
+        count += 1
+    for i in range(3, int(math.sqrt(n))+1, 2):
+        while n % i == 0:
+            n = n / i
+            count += 1
+    if n > 2:
+        count += 1
+    return count
 
 
-def strange_number():
-    X, K = map(int, stdin.readline().split())
-    total_di = 0
-    if K == 1:
-        if X > 1:
+for _ in range(int(sys.stdin.readline())):
+    x, k = map(int, sys.stdin.readline().split())
+    if x == 1:
+        if k == 0:
             print("1")
-            return
         else:
             print("0")
-            return
-    if X == 1:
-        if K == 0:
-            print("1")
-            return
-        else:
-            print("0")
-            return
+    elif k <= primeFactors(x):
+        print(1)
     else:
-        i = 1
-        while X != 1:
-            i += 1
-            if isPrime(i):
-                while X % i == 0:
-                    X = X // i
-                    total_di += 1
-                    if K <= total_di:
-                        print("1")
-                        return 0
-
-        if K <= total_di:
-            print("1")
-            return
-        else:
-            print("0")
-            return
-
-
-T = int(stdin.readline())
-for t in range(T):
-    strange_number()
-
-'''
-
-'''
+        print(0)
