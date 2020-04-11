48f1efc6e6362db6a089c13fece775af9cbeed1a
Prajjwal Datir <46681482+PrajjwalDatir@users.noreply.github.com>
2020-04-12 00:28:13 +0530

    got 20/100
    TLE

--- Square.cpp
+++ Square.cpp
@@ -1,12 +1,9 @@
 #include<bits/stdc++.h>
-  
 using namespace std;
-
 int main(){
     long T, N;
     cin >> T;
-    while(T--)
-    {
+    while(T--){
         cin >> N;
         long my_array[N], mul_arr[N];
         long i = 0, j = 0;
@@ -14,54 +11,24 @@ int main(){
         long my_iter = 0;
         while(i < N)
             cin >> my_array[i++];
-
-				//if (mul_arr[0] % 4 != 2){
-				//	cnt++;
-				//}
-		for (i = 0; i < N; i++)
-		{
+		for (i = 0; i < N; i++){
 			mul_arr[i] = abs(my_array[i]) % 4;
-		//cout << mul_arr[i];
 		}
-		//cout << endl;
-		
 		for (my_iter = 0; my_iter < N; my_iter++){
-		// cout << my_iter << endl;
-			for (i = my_iter; i < N; i++)
-			{
+			for (i = my_iter; i < N; i++){
 				if (mul_arr[i] == 0){
 					cnt = cnt + N - i;
 					break;
 				}
-				if (mul_arr[i] % 4 == 1){
-					while ((mul_arr[i] != 2) && (i < N)){
-						cnt++;
-						i++;
-					}
-					if (i == N){
-						break;
-					}
+				else if ((mul_arr[i] == 1) || (mul_arr[i] == 3)){
+					cnt++;
 				}
-				if (mul_arr[i] % 4 == 3){
-					while (mul_arr[i] != 2 && i < N){
-						cnt++;
+				else if (mul_arr[i] == 2){
+					while (i < N - 1){
 						i++;
-					}
-					if (i == N){
-						break;
-					}	
-				}
-				if (mul_arr[i] == 2){
-					while (i < N - 1)
-					{
-						i++;
-						if (mul_arr[i] == 2){
+						if ((mul_arr[i] == 2) || (mul_arr[i] == 0)){
 							cnt = cnt + N - i;
 							break;
-						}
-						if (mul_arr[i] == 0){
-							cnt = cnt + N - i;
-							break;	
 						}
 					}
 					break;
