0b39b8416b3dd8d5ba3cd7bd7eaf2213c3aeff56
Prajjwal Datir <46681482+PrajjwalDatir@users.noreply.github.com>
2020-04-12 00:30:41 +0530

    20/100
    O(n) is n^2 need to reduce it.

--- SquareSub.py
+++ SquareSub.py
@@ -1,35 +1,30 @@
 from sys import stdin
 
 
-def difSquare(n):
-    return n % 4 != 2
-
-
-def just_give_me_damn_output(seq, lengthhh):
-    ans = 0
-    # let's seq = [2,5,6]
-    for i in range(lengthhh):  # for i to 0 to 3 means 0,1,2
-        someNum = 1
-        for j in range(i, lengthhh):  # j will be [0,1,2], [1, 2], [2]
-            someNum *= abs(seq[j])
-            # print(seq[k], end=" ")
-            # print(someNum)
-            if difSquare(someNum):
-                ans += 1
-    print(ans)
-
-
-def square_sub():
-    # count = 0  # to output the answer
+T = int(stdin.readline())
+for _ in range(T):
     seq_size = int(stdin.readline())
     # pass all the combination of given sequence
     sequence = list(map(int, stdin.readline().split()))
-    # need to find all combinations of the sequence
-    # length = len(sequence)
-    # myPowerSet(sequence, seq_size)
-    just_give_me_damn_output(sequence, seq_size)
-
-
-T = int(stdin.readline())
-for t in range(T):
-    square_sub()
+    mul_arr = [None]*seq_size
+    cnt = 0
+    for i in range(0, seq_size):
+        mul_arr[i] = abs(sequence[i]) % 4
+    # print(mul_arr)
+    for my_iter in range(seq_size):
+        for i in range(my_iter, seq_size):
+            if mul_arr[i] == 0:
+                cnt = cnt + seq_size - i
+                break
+            elif mul_arr[i] == 1:  # 0 2 1 1 1 1 3 3
+                cnt += 1  # 8 + 6 + 5 + 4 + 3 + 2 + 1
+            elif mul_arr[i] == 3:
+                cnt += 1
+            elif mul_arr[i] == 2:
+                while i < seq_size - 1:
+                    i += 1
+                    if mul_arr[i] == 2 or mul_arr[i] == 0:
+                        cnt = cnt + seq_size - i
+                        break
+                break
+    print(cnt)
