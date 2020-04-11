0f6b247b81154e10fa2a42fa141df4ffb001d76d
Prajjwal Datir <46681482+PrajjwalDatir@users.noreply.github.com>
2020-04-11 19:14:43 +0530

    Square.cpp 1.0.0

--- Square.cpp
+++ Square.cpp
@@ -5,35 +5,67 @@ using namespace std;
 int main(){
     long T, N;
     cin >> T;
-    while(T--){
+    while(T--)
+    {
         cin >> N;
         long my_array[N], mul_arr[N];
         long i = 0, j = 0;
         long cnt = 0;
+        long my_iter = 0;
         while(i < N)
             cin >> my_array[i++];
 
-				mul_arr[0] = my_array[0];
-				if (mul_arr[0] % 4 != 2){
-					cnt++;
+				//if (mul_arr[0] % 4 != 2){
+				//	cnt++;
+				//}
+		for (i = 0; i < N; i++)
+		{
+			mul_arr[i] = abs(my_array[i]) % 4;
+		//cout << mul_arr[i];
+				  //0 case not handled
+		}
+		//cout << endl;
+		
+		for (my_iter = 0; my_iter < N; my_iter++){
+		// cout << my_iter << endl;
+			for (i = my_iter; i < N; i++)
+			{
+				if (mul_arr[i] == 0){
+					cnt = cnt + N - i;
+					break;
 				}
-
-				for (i = 1; i < N; i++){
-			        mul_arr[i] = my_array[i] * mul_arr[i - 1];
-			  //mul array is right!
+				if (mul_arr[i] % 4 == 1){
+					while ((mul_arr[i] != 2) && (i < N)){
+						cnt++;
+						i++;
+					}
+					if (i == N){
+						break;
+					}
 				}
-				
-				for (i = 1; i < N; i++){
-					for (j = i; j < N; j++){
-						if ((mul_arr[j] / mul_arr[i - 1]) % 4 != 2){
-							cnt++;
+				if (mul_arr[i] % 4 == 3){
+					while (mul_arr[i] != 2 && i < N){
+						cnt++;
+						i++;
+					}
+					if (i == N){
+						break;
+					}	
+				}
+				if (mul_arr[i] == 2){
+					while (i < N - 1)
+					{
+						i++;
+						if (mul_arr[i] == 2){
+							cnt = cnt + N - i;
+							break;
 						}
 					}
-					if (mul_arr[i] % 4 != 2){
-						cnt++;
-					}
-				}
-		cout << cnt << endl;
+					break;
+				}	
+			}	
 		}
+		cout << cnt << endl;
+	}
     return 0;
 }
